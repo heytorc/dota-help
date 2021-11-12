d511bfa67378a103606d505ec21f938e4eab5070
Heytor Cavalcanti <heytor.c.moura@accenture.com>
2021-11-11 23:23:58 -0300

    feat: added steam service

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+
+import SteamService from 'src/services/steam.services';
+
 import { ReactComponent as Logo } from 'src/assets/images/logo.svg';
 
 import Container from 'src/styles/components/Container';
 
 const Home: React.FC = () => {
+
+  const steamAuth = async () => {
+    const redirectUrl = await SteamService.getRedirectUrl();
+    console.log(redirectUrl);
+  };
+
   return (
     <Container>
       <Logo />
+
+      <button onClick={steamAuth}>Entrar</button>
     </Container>
   );
 }
